8c349773d1df551ac36299336de92754378331e8
Arnau <arnaucb01@gmail.com>
2021-02-22 20:11:01 +0100

    mostrar habilitats random

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -6,7 +6,9 @@ import axios from 'axios';
 class App extends Component {
   state = {
     nom:'',
-    imatge:''
+    imatge:'',
+    habilitats:[],
+    pokemon:{}
   }
 
   componentDidMount(){
@@ -21,13 +23,25 @@ class App extends Component {
       console.log(pokemon.sprites.front_default);
       this.setState({imatge:pokemon.sprites.front_default})
       this.setState({nom:pokemon.name})
+      this.setState({pokemon : pokemon})
+      this.getHabilitats(pokemon)
     })
   }
+  getHabilitats(pokemon){
+    let  aux = pokemon.moves;
+    let moviments = [];
+    for (let i = 0; i < 4; i++) {
+      moviments[i] = aux[Math.floor(Math.random() * aux.length)] 
+    }
+    this.setState({habilitats : moviments})
+    console.log();
+  }  
 
   render() {
     return (
      <div>
        <p>{this.state.nom.toUpperCase()}</p>
+       <p>{this.state.habilitats[0]?.move.name}</p>
       <img src={this.state.imatge} />
      </div>
     );
