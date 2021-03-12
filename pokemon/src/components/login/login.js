32e3e1d27ba59a4f0f9c783edb12473299f6ad4f
Aleix Soler <aleixsoler123@gmail.com>
2021-03-12 19:34:37 +0100

    login

--- pokemon/src/components/login/login.js
+++ pokemon/src/components/login/login.js
@@ -8,7 +8,12 @@ class login extends Component{
         let pass = document.getElementById('password');
         let response = await fetch("http://172.24.4.216:5000/login?user="+user.value+"&pass="+pass.value);
         let json = await response.json();
-        console.log("LOGIN=>",json);
+        if(json["userid"]!=0){
+            console.log("LOGIN=>",json);
+        }else{
+            this.errorFunction("No existeix cap usuari amb aquest Usuari/Contrasenya");
+        }
+        
     }
     
     async registerPetition(){
@@ -16,16 +21,30 @@ class login extends Component{
         let pass = document.getElementById('password');
         let response = await fetch("http://172.24.4.216:5000/register?user="+user.value+"&pass="+pass.value);
         let json = await response.json();
-        console.log("REGISTER=>",json);
+        if(json["userCreated"]==1){
+            console.log("REGISTER=>",json);
+        }else{
+            this.errorFunction("Ja existeix un usuari amb aquest nom")
+        }
+
+    }
+
+    errorFunction(frase){
+        var p = document.getElementById("errors");
+        p.innerHTML=frase;
+        setTimeout(() => {
+            p.innerHTML = "";
+        }, 5000);
     }
 
     render(){
         return(
             <div class="table">
-                <p>Username:<input id="username" type="text"></input></p>
-                <p>Password:<input id="password" type="password"></input></p>
+                <p id="user">Username:<input id="username" type="text"></input></p>
+                <p id="pass">Password:<input id="password" type="password"></input></p>
                 <button onClick={() => this.loginPetition()}>LOGIN</button>
                 <button onClick={() => this.registerPetition()}>REGISTER</button>
+                <p id="errors"></p>
             </div>
         )
     }
